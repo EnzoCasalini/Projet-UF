4b4660bfc3aa5e5932eecaf44ece795545354652
hugolanzafameynov <hugo.lanzafame@ynov.com>
2023-05-02 15:26:14 +0200

    connexion avec firebase pour l'auth

--- Appli/GameWatcher/Navigation/Pages/LoginRegisterPage/LoginPage/LoginPage.jsx
+++ Appli/GameWatcher/Navigation/Pages/LoginRegisterPage/LoginPage/LoginPage.jsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TextInput, View, Pressable} from 'react-native';
+import {auth} from '../../../../firebaseConfig' ;
+import {signInWithEmailAndPassword} from "firebase/auth";
 
 const LoginPage = ({navigation}) => {
-    const [email, onChangeEmail] = React.useState('');
-    const [password, onChangePassword] = React.useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     const handleLogin = () => {
-        navigation.navigate('Home');
-        navigation.reset({
-            index: 0,
-            routes: [{ name: 'Home' }],
-        });
+        // Reset all errors
+        setEmailError('');
+        setPasswordError('');
+        // Reset password
+        setPassword('');
 
+        // Validate form
+        let valid = true;
+        if (!email) {
+            setEmailError('Veuillez entrer votre email');
+            valid = false;
+        }
+        if (!password) {
+            setPasswordError('Veuillez entrer votre mot de passe');
+            valid = false;
+        }
+
+        // If form is valid, create the account
+        if (valid) {
+            signInWithEmailAndPassword(auth, email, password)
+                .then(() => {
+                    navigation.reset({
+                        index: 0,
+                        routes: [{name: 'Home'}],
+                    });
+                })
+                .catch((error) => {
+                    alert(error.message);
+                });
+        }
     }
 
     const goToRegister = () => navigation.navigate('Register');
 
     const goToHome = () => {
-        navigation.navigate('Home');
         navigation.reset({
             index: 0,
-            routes: [{ name: 'Home' }],
+            routes: [{name: 'Home'}],
         });
     }
 
@@ -28,25 +56,28 @@ const LoginPage = ({navigation}) => {
         <View style={styles.loginPage}>
             <Text style={styles.loginTitle}>Connexion</Text>
             <TextInput
-                style={styles.loginInput}
-                onChangeText={onChangeEmail}
+                style={[styles.loginInput, passwordError && styles.loginInputError]}
+                onChangeText={setEmail}
                 value={email}
                 placeholder="Email"
             />
+            <Text style={styles.loginError}>{emailError}</Text>
             <TextInput
-                style={styles.loginInput}
-                onChangeText={onChangePassword}
+                style={[styles.loginInput, passwordError && styles.loginInputError]}
+                onChangeText={setPassword}
+                secureTextEntry={true}
                 value={password}
                 placeholder="Mot de passe"
             />
+            <Text style={styles.loginError}>{passwordError}</Text>
             <Pressable style={styles.loginButton} onPress={handleLogin}>
                 <Text>Se connecter</Text>
             </Pressable>
-            <View style={styles.loginBottomButton}>
-                <Pressable onPress={goToRegister}>
+            <View style={styles.loginBottomContainer}>
+                <Pressable style={styles.loginBottomButton} onPress={goToRegister}>
                     <Text>Creer un compte</Text>
                 </Pressable>
-                <Pressable onPress={goToHome}>
+                <Pressable style={styles.loginBottomButton} onPress={goToHome}>
                     <Text>Continuer en tant qu'invité</Text>
                 </Pressable>
             </View>
@@ -72,6 +103,15 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 15,
     },
+    loginInputError: {
+        borderColor: 'red',
+    },
+    loginError: {
+        color: 'red',
+        fontSize: 12,
+        height: 12,
+        marginTop: -10,
+    },
     loginButton: {
         alignItems: 'center',
         justifyContent: 'center',
@@ -82,10 +122,14 @@ const styles = StyleSheet.create({
         backgroundColor: 'red',
         margin: 50,
     },
-    loginBottomButton: {
+    loginBottomContainer: {
         alignItems: 'center',
+        position: 'absolute',
+        bottom: 75,
+    },
+    loginBottomButton: {
+        padding: 5,
     },
 });
-
 
 export default LoginPage;
--- Appli/GameWatcher/Navigation/Pages/LoginRegisterPage/RegisterPage/RegisterPage.jsx
+++ Appli/GameWatcher/Navigation/Pages/LoginRegisterPage/RegisterPage/RegisterPage.jsx
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TextInput, View, Pressable} from 'react-native';
+import {auth} from '../../../../firebaseConfig';
+import {createUserWithEmailAndPassword} from "firebase/auth";
 
 const RegisterPage = ({navigation}) => {
-    const [username, onChangeUsername] = React.useState('');
-    const [email, onChangeEmail] = React.useState('');
-    const [password, onChangePassword] = React.useState('');
-    const [confirmPassword, onChangeConfirmPassword] = React.useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleRegister= () => {
-        navigation.navigate('Home');
-        navigation.reset({
-            index: 0,
-            routes: [{ name: 'Home' }],
-        });
+    const [usernameError, setUsernameError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
+
+    const handleRegister = () => {
+        // Reset all errors
+        setUsernameError('')
+        setEmailError('');
+        setPasswordError('');
+        setConfirmPasswordError('');
+        // Reset password
+        setPassword('');
+        setConfirmPassword('');
+
+        // Validate form
+        let valid = true;
+        if (!username) {
+            setUsernameError('Veuillez entrer votre nom d\'utilisateur');
+            valid = false;
+        }
+        if (!email) {
+            setEmailError('Veuillez entrer votre email');
+            valid = false;
+        }
+        if (!password) {
+            setPasswordError('Veuillez entrer votre mot de passe');
+            valid = false;
+        }
+        if (!confirmPassword) {
+            setConfirmPasswordError('Veuillez confirmer votre mot de passe');
+            valid = false;
+        } else if (password.length < 6) {
+            setPasswordError('Le mot de passe doit contenir un minimum de 6 characteres');
+        }
+        if (password !== confirmPassword) {
+            setPasswordError('Les mots de passe ne correspondent pas');
+            valid = false;
+        }
+
+        // If form is valid, create the account
+        if (valid) {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then(() => {
+                    navigation.navigate('Login')
+                })
+                .catch((error) => {
+                    alert(error.message);
+                });
+        }
     }
 
     const goToLogin = () => navigation.navigate('Login');
@@ -29,37 +75,43 @@ const RegisterPage = ({navigation}) => {
         <View style={styles.loginPage}>
             <Text style={styles.loginTitle}>Inscription</Text>
             <TextInput
-                style={styles.loginInput}
-                onChangeText={onChangeUsername}
+                style={[styles.loginInput, usernameError && styles.loginInputError]}
+                onChangeText={setUsername}
                 value={username}
                 placeholder="Nom d'utilisateur"
             />
+            <Text style={styles.loginError}>{usernameError}</Text>
             <TextInput
-                style={styles.loginInput}
-                onChangeText={onChangeEmail}
+                style={[styles.loginInput, emailError && styles.loginInputError]}
+                onChangeText={setEmail}
                 value={email}
                 placeholder="Email"
             />
+            <Text style={styles.loginError}>{emailError}</Text>
             <TextInput
-                style={styles.loginInput}
-                onChangeText={onChangePassword}
+                style={[styles.loginInput, passwordError && styles.loginInputError]}
+                onChangeText={setPassword}
+                secureTextEntry={true}
                 value={password}
                 placeholder="Mot de passe"
             />
+            <Text style={styles.loginError}>{passwordError}</Text>
             <TextInput
-                style={styles.loginInput}
-                onChangeText={onChangeConfirmPassword}
+                style={[styles.loginInput, confirmPasswordError && styles.loginInputError]}
+                onChangeText={setConfirmPassword}
+                secureTextEntry={true}
                 value={confirmPassword}
                 placeholder="Confirmer le mot de passe"
             />
+            <Text style={styles.loginError}>{confirmPasswordError}</Text>
             <Pressable style={styles.loginButton} onPress={handleRegister}>
                 <Text>Se connecter</Text>
             </Pressable>
-            <View style={styles.loginBottomButton}>
-                <Pressable onPress={goToLogin}>
+            <View style={styles.loginBottomContainer}>
+                <Pressable style={styles.loginBottomButton} onPress={goToLogin}>
                     <Text>Creer un compte</Text>
                 </Pressable>
-                <Pressable onPress={goToHome}>
+                <Pressable style={styles.loginBottomButton} onPress={goToHome}>
                     <Text>Continuer en tant qu'invité</Text>
                 </Pressable>
             </View>
@@ -85,6 +137,15 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 15,
     },
+    loginInputError: {
+        borderColor: 'red',
+    },
+    loginError: {
+        color: 'red',
+        fontSize: 12,
+        height: 12,
+        marginTop: -10,
+    },
     loginButton: {
         alignItems: 'center',
         justifyContent: 'center',
@@ -95,8 +156,13 @@ const styles = StyleSheet.create({
         backgroundColor: 'red',
         margin: 50,
     },
-    loginBottomButton: {
+    loginBottomContainer: {
         alignItems: 'center',
+        position: 'absolute',
+        bottom: 75,
+    },
+    loginBottomButton: {
+        padding: 5,
     },
 });
 
