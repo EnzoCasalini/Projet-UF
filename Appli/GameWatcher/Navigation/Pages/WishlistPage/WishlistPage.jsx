b23cec4df29b8320ab16a18cef3cc89e9b8fdb96
EnzoCasalini <enzocasa83@gmail.com>
2023-05-21 22:39:51 +0200

    Page Wishlist grisée si l'user n'est pas connecté.

--- Appli/GameWatcher/Navigation/Pages/WishlistPage/WishlistPage.jsx
+++ Appli/GameWatcher/Navigation/Pages/WishlistPage/WishlistPage.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import FavoriteGamesList from "./FavoriteGamesList/FavoriteGamesList";
+import {auth} from "../../../firebaseConfig";
 
 const WishlistPage = ({navigation}) => {
+    const userId = auth.currentUser ? auth.currentUser.uid : null;
+
     return (
         <View style={styles.gamesContainer}>
             <FavoriteGamesList navigation={navigation} />
+            {!userId && (
+                <View style={styles.loginMessageContainer}>
+                    <Text style={styles.loginMessageText}>Veuillez vous connecter pour utiliser cette fonctionnalité</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                        <Text style={styles.loginLink}>Se connecter</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
         </View>
     );
 };
@@ -18,6 +29,28 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
         zIndex: 1,
         backgroundColor: "#242429"
+    },
+    loginMessageContainer: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.4)",
+        paddingTop: "70%",
+        paddingHorizontal: "10%",
+        alignItems: "center",
+    },
+    loginMessageText: {
+        color: "#C9FAE8",
+        fontSize: 18,
+        textAlign: "center",
+    },
+    loginLink: {
+        color: '#4EF5B9',
+        fontSize: 16,
+        textDecorationLine: 'underline',
+        marginTop: 10,
     },
 });
 
